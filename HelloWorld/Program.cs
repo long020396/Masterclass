63d2a80622ae91374d228fbabdac0d2cbf58d95a
Long Du <long020396@gmail.com>
2020-08-02 23:34:49 +0700

    Data type int, float and double

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 / d2;
+
+            float f1 = 3.5F;
+
+
+            Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, sum);
+            Console.WriteLine("The sum of {0} and {1} is {2}", d1, d2, sumD);
+            Console.Read();
         }
     }
 }
